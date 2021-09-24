07087f8320086964f9a1b5a7bdf3c6d8314a8f6c
Ibrahim Abdullahi Aliyu <Ibrahimabul050@gmail.com>
2021-09-24 22:41:21 +0100

    Adds JS docs and extends functionality

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-module.exports = Array.prototype.asyncForEach = async function (callback) {
-  for (let i = 0; i < this.length -1; i++) {
-    await callback(this[i])
+/**
+ * Adds an asynchronous prototype method to an Array object.
+ * @param {Function} required The function to perform on each item in the array.
+ * The function can accept any number of parameters.
+ * 
+ * When the callback is invoked, it passes all the params as passed with additional of two params [index, array].
+ * @var index is the index of the item in the array.
+ * @var array is the array the method is being called on.
+ */
+module.exports = Array.prototype.asyncForEach = async function (callback, ...params) {
+  for (let index = 0; index < this.length -1; index++) {
+    await callback(this[index], ...params, index, this);
   }
 }
